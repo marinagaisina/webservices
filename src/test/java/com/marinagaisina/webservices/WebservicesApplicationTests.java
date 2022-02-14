33766fd169ba7e936ec8df81672a7561172aea1d
Marina Gaisina <farigatovna@hotmail.com>
2022-02-14 14:15:04 -0500

    Initial commit

---
+++ src/test/java/com/marinagaisina/webservices/WebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marinagaisina.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
